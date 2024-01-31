b6e1bd732fba9d55d1c6dce9a60579701b1fd53e
Samuel Défago <defagos@gmail.com>
2024-01-31 17:21:12 +0100

    Cleanup temporary code

--- Sources/Player/Internal/QueuePlayer.swift
+++ Sources/Player/Internal/QueuePlayer.swift
@@ -159,7 +159,6 @@ extension AVQueuePlayer {
         if let firstItem = items.first {
             if firstItem !== self.items().first {
                 remove(firstItem)
-                print("--> replace current")
                 replaceCurrentItem(with: firstItem)
             }
             removeAll(from: 1)
