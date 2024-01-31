3237709f5c04d9b1a7b0ca2e9de91803f5a033ab
Samuel Défago <defagos@gmail.com>
2024-01-31 10:56:52 +0100

    Prepare item loading mechanism

--- Sources/Player/PlayerItem.swift
+++ Sources/Player/PlayerItem.swift
@@ -6,6 +6,7 @@
 
 import AVFoundation
 import Combine
+import PillarboxCore
 
 /// This class represents a playable item that can be inserted into a ``Player``.
 ///
@@ -17,6 +18,8 @@ import Combine
 ///
 /// - Note: You can also create your own ``PlayerItem`` by extending the class.
 public final class PlayerItem: Equatable {
+    private static let trigger = Trigger()
+
     @Published private(set) var asset: any Assetable
 
     private let id = UUID()
@@ -24,14 +27,17 @@ public final class PlayerItem: Equatable {
     /// Creates the item from an ``Asset`` publisher data source.
     public init<P, M>(publisher: P, trackerAdapters: [TrackerAdapter<M>] = []) where P: Publisher, M: AssetMetadata, P.Output == Asset<M> {
         asset = Asset<M>.loading.withId(id).withTrackerAdapters(trackerAdapters)
-        publisher
-            .catch { error in
-                Just(.failed(error: error))
-            }
-            .map { [id] asset in
-                asset.withId(id).withTrackerAdapters(trackerAdapters)
-            }
-            .assign(to: &$asset)
+        Publishers.Publish(onOutputFrom: Self.trigger.signal(activatedBy: id)) {
+            publisher
+                .catch { error in
+                    Just(.failed(error: error))
+                }
+        }
+        .map { [id] asset in
+            asset.withId(id).withTrackerAdapters(trackerAdapters)
+        }
+        .assign(to: &$asset)
+        load()
     }
 
     /// Creates a player item from an ``Asset``.
@@ -49,6 +55,10 @@ public final class PlayerItem: Equatable {
 
     func matches(_ playerItem: AVPlayerItem?) -> Bool {
         asset.matches(playerItem)
+    }
+
+    func load() {
+        Self.trigger.activate(for: id)
     }
 }
 
