6973e2bb7b0df5170e50a8c94a9484a099161d69
Samuel Défago <defagos@gmail.com>
2024-01-31 16:00:26 +0100

    Use loading resource loader delegate to trigger asset delivery

--- Sources/Player/ResourceLoading/LoadingResourceLoaderDelegate.swift
+++ Sources/Player/ResourceLoading/LoadingResourceLoaderDelegate.swift
@@ -7,11 +7,18 @@
 import AVFoundation
 
 final class LoadingResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate {
+    private let id: UUID
+
+    init(id: UUID) {
+        self.id = id
+    }
+
     func resourceLoader(
         _ resourceLoader: AVAssetResourceLoader,
         shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest
     ) -> Bool {
-        true
+        PlayerItem.trigger.activate(for: id)
+        return true
     }
 
     func resourceLoader(
