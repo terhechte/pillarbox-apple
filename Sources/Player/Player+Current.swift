cc2a6a59b02bec5af2d96064a940c483481d1668
Samuel Défago <defagos@gmail.com>
2024-02-19 11:27:09 +0100

    Extract sub-publisher

--- Sources/Player/Player+Current.swift
+++ Sources/Player/Player+Current.swift
@@ -10,6 +10,27 @@ import PillarboxCore
 
 extension Player {
     func queueItemsPublisher() -> AnyPublisher<[AVPlayerItem], Never> {
+        queuePublisher()
+            .withPrevious(ItemQueue.initial)
+            .compactMap { [configuration] previous, current in
+                switch current.itemTransition {
+                case let .advance(item):
+                    return AVPlayerItem.playerItems(
+                        for: current.assets,
+                        replacing: previous.assets,
+                        currentItem: item,
+                        length: configuration.preloadedItems
+                    )
+                case let .stop(item):
+                    return [item]
+                case .finish:
+                    return nil
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private func queuePublisher() -> AnyPublisher<ItemQueue, Never> {
         Publishers.Merge(
             assetsPublisher()
                 .map { ItemQueueUpdate.assets($0) },
@@ -18,22 +39,6 @@ extension Player {
         )
         .scan(ItemQueue.initial) { queue, update in
             queue.updated(with: update)
-        }
-        .withPrevious(ItemQueue.initial)
-        .compactMap { [configuration] previous, current in
-            switch current.itemTransition {
-            case let .advance(item):
-                return AVPlayerItem.playerItems(
-                    for: current.assets,
-                    replacing: previous.assets,
-                    currentItem: item,
-                    length: configuration.preloadedItems
-                )
-            case let .stop(item):
-                return [item]
-            case .finish:
-                return nil
-            }
         }
         .eraseToAnyPublisher()
     }
