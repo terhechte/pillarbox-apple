9e5690a50a4cc519441dccbb5c706752623a362c
Benedikt Terhechte <terhechte@gmail.com>
2024-03-15 09:45:43 +0100

    remove the analytics junk

--- Package.swift
+++ Package.swift
@@ -10,20 +10,12 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "PillarboxAnalytics",
-            targets: ["PillarboxAnalytics"]
-        ),
-        .library(
             name: "PillarboxCircumspect",
             targets: ["PillarboxCircumspect"]
         ),
         .library(
             name: "PillarboxCore",
             targets: ["PillarboxCore"]
-        ),
-        .library(
-            name: "PillarboxCoreBusiness",
-            targets: ["PillarboxCoreBusiness"]
         ),
         .library(
             name: "PillarboxPlayer",
@@ -36,42 +28,12 @@ let package = Package(
         .package(url: "https://github.com/CommandersAct/iOSV5.git", .upToNextMinor(from: "5.4.4")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.3")),
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", exact: "1.0.1"),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "13.0.0")),
-        .package(url: "https://github.com/icanzilb/TimelaneCombine.git", .upToNextMajor(from: "2.0.0"))
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "13.0.0"))
     ],
     targets: [
         .target(
-            name: "PillarboxAnalytics",
-            dependencies: [
-                .target(name: "PillarboxPlayer"),
-                .product(name: "ComScore", package: "Comscore-Swift-Package-Manager"),
-                .product(name: "TCCore", package: "iOSV5"),
-                .product(name: "TCServerSide", package: "iOSV5")
-            ],
-            path: "Sources/Analytics",
-            linkerSettings: [
-                .linkedFramework("AdSupport")
-            ],
-            plugins: [
-                .plugin(name: "PackageInfoPlugin")
-            ]
-        ),
-        .target(
             name: "PillarboxCore",
             path: "Sources/Core"
-        ),
-        .target(
-            name: "PillarboxCoreBusiness",
-            dependencies: [
-                .target(name: "PillarboxAnalytics")
-            ],
-            path: "Sources/CoreBusiness",
-            resources: [
-                .process("Resources")
-            ],
-            plugins: [
-                .plugin(name: "PackageInfoPlugin")
-            ]
         ),
         .target(
             name: "PillarboxCircumspect",
@@ -86,8 +48,7 @@ let package = Package(
             dependencies: [
                 .target(name: "PillarboxCore"),
                 .product(name: "CombineExt", package: "CombineExt"),
-                .product(name: "DequeModule", package: "swift-collections"),
-                .product(name: "TimelaneCombine", package: "TimelaneCombine")
+                .product(name: "DequeModule", package: "swift-collections")
             ],
             path: "Sources/Player",
             resources: [
@@ -113,14 +74,6 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "AnalyticsTests",
-            dependencies: [
-                .target(name: "PillarboxAnalytics"),
-                .target(name: "PillarboxCircumspect"),
-                .target(name: "PillarboxStreams")
-            ]
-        ),
-        .testTarget(
             name: "CircumspectTests",
             dependencies: [
                 .target(name: "PillarboxCircumspect")
@@ -131,13 +84,6 @@ let package = Package(
             dependencies: [
                 .target(name: "PillarboxCircumspect"),
                 .target(name: "PillarboxCore")
-            ]
-        ),
-        .testTarget(
-            name: "CoreBusinessTests",
-            dependencies: [
-                .target(name: "PillarboxCircumspect"),
-                .target(name: "PillarboxCoreBusiness")
             ]
         ),
         .testTarget(
